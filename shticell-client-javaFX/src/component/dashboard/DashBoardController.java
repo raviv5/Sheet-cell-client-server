3877a8891a0c5937ac10dd2d1d691d51e37c9682
raviv5 <itayraviv5@gmail.com>
2024-10-20 10:13:25 +0300

    init dashboard

---
+++ shticell-client-javaFX/src/component/dashboard/DashBoardController.java
@@ -0,0 +1,70 @@
+package component.dashboard;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableView;
+
+public class DashBoardController {
+
+    @FXML
+    private Button confirmButton;
+
+    @FXML
+    private Button denyButton;
+
+    @FXML
+    private Button loadSheetButton;
+
+    @FXML
+    private Button requestPermissionButton;
+
+    @FXML
+    private TableView<?> requestTableView;
+
+    @FXML
+    private TableView<?> sheetTableView;
+
+    @FXML
+    private Button viewSheetButton;
+
+    @FXML
+    void confirmAction(ActionEvent event) {
+
+    }
+    @FXML
+    void initialize() {
+
+    }
+
+    @FXML
+    void denyAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void loadSheetAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void requestPermissionAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void requestTableViewOnSort(ActionEvent event) {
+
+    }
+
+    @FXML
+    void sheetTableViewOnSort(ActionEvent event) {
+
+    }
+
+    @FXML
+    void viewSheetAction(ActionEvent event) {
+
+    }
+
+}
